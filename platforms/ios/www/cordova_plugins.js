e7fafb73e82a049d2fa81d75656d3b9ed8f21e98
Jonathan <jonathan.inf@outlook.com>
2018-10-03 12:56:02 -0300

    apk

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,53 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "card.io.cordova.mobilesdk.CardIO",
+    "file": "plugins/card.io.cordova.mobilesdk/www/cdv-plugin-card-io.js",
+    "pluginId": "card.io.cordova.mobilesdk",
+    "clobbers": [
+      "CardIO"
+    ]
+  },
+  {
+    "id": "com.paypal.cordova.mobilesdk.PayPalMobile",
+    "file": "plugins/com.paypal.cordova.mobilesdk/www/cdv-plugin-paypal-mobile-sdk.js",
+    "pluginId": "com.paypal.cordova.mobilesdk",
+    "clobbers": [
+      "PayPalMobile"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-keyboard.keyboard",
+    "file": "plugins/cordova-plugin-ionic-keyboard/www/ios/keyboard.js",
+    "pluginId": "cordova-plugin-ionic-keyboard",
+    "clobbers": [
+      "window.Keyboard"
+    ]
+  },
+  {
+    "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "cordova.exec"
+    ]
+  },
+  {
+    "id": "cordova-plugin-spl
