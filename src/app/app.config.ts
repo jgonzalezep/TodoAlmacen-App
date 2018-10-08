efbe0a7440ad617073546064d1cd95e074f2096c
Jonathan <jonathan.inf@outlook.com>
2018-10-08 01:20:39 -0300

    rest api flow

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,6 +14,9 @@ export interface AppConfig {
 	paypalProduction: string;
 	payuSalt: string;
 	payuKey: string;
+	APIKEY: string;
+	SECRETKEY: string;
+	APIURL: string;
 }
 
 export const BaseAppConfig: AppConfig = {
@@ -25,6 +28,10 @@ export const BaseAppConfig: AppConfig = {
 	adminUsername: "admin",
 	adminPassword: "qwertyuiop",
 	paypalSandbox: "",
+	APIURL: "https://flow.tuxpan.com/api",
+	APIKEY: "1F1B8561-E21E-4DC8-AB2B-23LE2FC1DF14",
+	SECRETKEY: "42b4e3846b0cf08c2e89406db3bfe72ad1e6787a",
+	
 	paypalProduction: "https://www.flow.cl/api",
 	payuSalt: "5C486FFD-CBD9-4406-B99C-1L2D94617D82",
 	payuKey: "d9e1f4069d3d8a40fc92e03663848d3721893b35"
